df0ced9d42b2ff06c88969e43030c88c2e1e3b1f
Mallikarjunarao  Vutukuri <mvutukuri2@asiapac.globalcsc.net>
2019-01-17 17:26:28 +0530

    new Test case

--- src/test/java/com/hospital/HospitalApplicationTests.java
+++ src/test/java/com/hospital/HospitalApplicationTests.java
@@ -5,12 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HospitalApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void dummy() {
+		assertTrue(1==1);
 	}
 
 }
